b4f634ac95532e2b3a42d728bcfdbaa71f98f930
Nicholas Deas <nickid0419@gmail.com>
2019-09-15 19:42:10 -0400

    Updated install bash script

--- src/VimAssemblySynInstaller.sh
+++ src/VimAssemblySynInstaller.sh
@@ -1,7 +1,7 @@
 echo "Beginning installation..."
 mkdir -p ~/.vim/{ftdetect, syntax}
-cp ./ftdetect/m.vim ~/.vim/ftdetect/m.vim
-cp ./syntax/m.vim ~/.vim/syntax/m.vim
+cp ./src/ftdetect/m.vim ~/.vim/ftdetect/m.vim
+cp ./src/syntax/m.vim ~/.vim/syntax/m.vim
 sed -i '/syntax on/d' ~/.vimrc
-cat ./vimrc_append.txt >> ~/.vimrc
+cat ./src/vimrc_append.txt >> ~/.vimrc
 echo "Completed Vim Assembly Syntax Highlighting Installation"
