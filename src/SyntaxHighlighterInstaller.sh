6a558bd32ba8f8893f212407d3f6fd375a301bdd
NickDeas <nickid0419@gmail.com>
2019-09-16 13:59:04 -0400

    Fixed Installer Completely

--- src/SyntaxHighlighterInstaller.sh
+++ src/SyntaxHighlighterInstaller.sh
@@ -1,4 +1,6 @@
-if $# == 0; then
+#!/bin/bash
+
+if [ $# == 0 ]; then
 	echo "Please specify which syntax highlighter you would like to install"
 	echo "\tAccumulator Machine: ACC"
 	echo "\tARM Assembly: ARM"
@@ -7,18 +9,18 @@ else
 
 	mkdir -p ~/.vim/ftdetect ~/.vim/syntax
 	
-	if "$1" == "ACC"; then
+	if [ "$1" == "ACC" ]; then
 		dos2unix ftdetect/m.vim
 		dos2unix syntax/m.vim
-		dos2unix ./vimrc_append.txt
+		dos2unix ./vimrc_append_m.txt
 		cp ./ftdetect/m.vim ~/.vim/ftdetect/m.vim
 		cp ./syntax/m.vim ~/.vim/syntax/m.vim
 		sed -i '/syntax on/d' ~/.vimrc
 		cat ./vimrc_append_m.txt >> ~/.vimrc
-	elif "$1" == "ARM"; then
+	elif [ "$1" == "ARM" ]; then
 		dos2unix ftdetect/s.vim
 		dos2unix syntax/s.vim
-		dos2unix ./vimrc_append.txt
+		dos2unix ./vimrc_append_s.txt
 		cp ./ftdetect/s.vim ~/.vim/ftdetect/s.vim
 		cp ./syntax/s.vim ~/.vim/syntax/s.vim
 		sed -i '/syntax on/d' ~/.vimrc
