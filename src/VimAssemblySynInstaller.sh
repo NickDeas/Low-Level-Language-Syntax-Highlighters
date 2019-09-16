2b2426f22fbf5af1633e00a5ae89fa8520fc1e6f
Nicholas Deas <nickid0419@gmail.com>
2019-09-15 20:06:36 -0400

    Update VimAssemblySynInstaller.sh

--- src/VimAssemblySynInstaller.sh
+++ src/VimAssemblySynInstaller.sh
@@ -1,5 +1,8 @@
 echo "Beginning installation..."
 mkdir -p ~/.vim/{ftdetect, syntax}
+dos2unix ftdetect/m.vim
+dos2unix syntax/m.vim
+dos2unix ./vimrc_append.txt
 cp ./ftdetect/m.vim ~/.vim/ftdetect/m.vim
 cp ./syntax/m.vim ~/.vim/syntax/m.vim
 sed -i '/syntax on/d' ~/.vimrc
